47b9e481eae7c844115345633a176b7015b8197f
DidacRomero <36265669+DidacRomero@users.noreply.github.com>
2018-12-06 21:56:42 +0100

    Exercise 1 "DONE" most functions don't test vars received :(

---
+++ Lab 3/Exercise 1/matRotToEulerAngles.m
@@ -0,0 +1,25 @@
+function [roll,pitch,yaw] = matRotToEulerAngles(rot_mat)
+%This function returns the euler rotation Angles found
+%in a rotation matrix.
+
+%First look for the pitch
+pitch = asin(-rot_mat(3,1));
+roll = atan2(rot_mat(3,2)/cos(pitch),rot_mat(3,3)/cos(pitch));
+yaw = atan2(rot_mat(2,1)/cos(pitch),rot_mat(1,1)/cos(pitch));
+
+%Display in degrees
+roll_disp = rad2deg(roll);
+pitch_disp = rad2deg(pitch);
+yaw_disp = rad2deg(yaw);
+
+%Display in Degrees
+disp('Roll: ');
+disp(roll_disp);
+disp('Pitch: ');
+disp(pitch_disp);
+disp('Yaw: ');
+disp(yaw_disp);
+end
+
+
+
