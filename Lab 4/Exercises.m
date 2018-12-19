37c96f628fc7aad66619726722ecab7a44ff7c93
DidacRomero <36265669+DidacRomero@users.noreply.github.com>
2018-12-20 00:51:12 +0100

    exercise 1 partially

--- Lab 4/Exercises.m
+++ Lab 4/Exercises.m
@@ -5,13 +5,26 @@ p_A = [3, 4]';
 p_B = [-2.5, 0.5]';
 % on a frame B
 
+%WE ASSUME
+ex1_o_A = [0 0]';
+
 %It is known that the angle between x axis of frame {A} and the y axes from
 %Frame{B} is 30 deg counterclockwise.
+ex1_rotMat_A_to_B = angleTo2dRotMat(30);
+ex1_rotMat_B_to_A = ex1_rotMat_A_to_B';
+
+%We isolate the translation
+ex_1_t_B_to_A = p_A - ex1_rotMat_B_to_A * p_B;
+ex_1_t_A_to_B = p_B - ex1_rotMat_A_to_B * p_A;
 
 % 1.1 Which are the coordinates of the origin of A seen from B?
-% 1.2 Which are the coordinates of the origin of B seen from A?
+ex1_oBA = ex1_rotMat_A_to_B * ex1_o_A + ex_1_t_A_to_B;
+% 1.2 Which are the coordinates of the origin of B seen from A? 
+%ASSUMINg the result above is ok
+ex1_oAB = ex1_o_A + t
 % 1.3 Which are the coordinates of a point q expressed in A if
 q_B = [3,1]'; %?
+ex_1_q_A = ex1_rotMat_B_to_A * q_B + ex_1_t_B_to_A;
 
 %% Exercise 2
 
